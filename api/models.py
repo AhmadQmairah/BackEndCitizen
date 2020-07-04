ed9f38fc12e7da201e23e6414cc7d4967fca824c
Ahmad Qmairah <AhmadQmairah@gmal.com>
2020-07-04 09:39:38 +0300

    3

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ class Issue(models.Model):
     description = models.CharField(max_length=300)
     name = models.CharField(max_length=300,)
     phone_number = models.CharField(max_length=12)
-    image = TextField()
+    image = models.TextField()
     lat = models.DecimalField(max_digits=5, decimal_places=2)
     lng = models.DecimalField(max_digits=5, decimal_places=2)
     created_date = models.DateTimeField(auto_now_add=True)
